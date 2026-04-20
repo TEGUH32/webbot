8673a876d36a8150d410e623f1f0ab5cd666140c
TEGUH32 <teguhmarwin99@gmail.com>
2026-04-20 17:30:38 +0700

    Create command.js

---
+++ api/command.js
@@ -0,0 +1,25 @@
+// api/commands.js
+import axios from 'axios'
+
+export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+    
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end()
+    }
+    
+    try {
+        const protocol = req.headers['x-forwarded-proto'] || 'https'
+        const host = req.headers['host']
+        const baseUrl = `${protocol}://${host}`
+        
+        const response = await axios.get(`${baseUrl}/api/bot-data`)
+        const data = response.data
+        
+        res.status(200).json(data.commands || [])
+    } catch (error) {
+        console.error('Error fetching commands:', error.message)
+        res.status(200).json([])
+    }
+}
